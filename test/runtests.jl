4f4cf247c54e4105f46858a394ff084b720f4b9d
ArchieCall <ArchieCall@users.noreply.github.com>
2015-09-23 19:48:29 -0400

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,1 +1,12 @@
-
+#--- tests are here sleep_ns()
+using Distributions
+using AccurateSleep
+import NewSleep.sleep_ns
+#using Base.test
+act_sleep_time = sleep_ns(.005)
+act_sleep_time = sleep_ns(.005)
+@show(act_sleep_time)
+test_times = [.006, .003, .001, .0001]
+for sleep_time in test_times
+  println(sleep_time)
+end
